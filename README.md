94f7139c7d036be40347961f1e13b05cd4bc5e3c
mugwort_rc <mugwort.rc@gmail.com>
2024-08-16 20:45:37 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# JMA Tiles Loader
+
+Based on https://qiita.com/e_toyoda/items/7a293313a725c4d306c0
+
---
+++ i18n/ja.ts
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../test/test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation>おはようございます</translation>
+    </message>
+</context>
+<context>
+    <name>JMATilesLoader</name>
+    <message>
+        <location filename="../jma_tiles_loader.py" line="177"/>
+        <source>&amp;JMA Tiles Loader</source>
+        <translation>&amp;JMA Tiles Loader</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader.py" line="164"/>
+        <source>JMA Tiles</source>
+        <translation>JMAタイル</translation>
+    </message>
+</context>
+<context>
+    <name>JMATilesLoaderDialog</name>
+    <message>
+        <location filename="../jma_tiles_loader_dialog.py" line="62"/>
+        <source>B13/TBB</source>
+        <translation>赤外画像</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog.py" line="62"/>
+        <source>B03/ALBD</source>
+        <translation>可視画像</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog.py" line="62"/>
+        <source>B08/TBB</source>
+        <translation>水蒸気画像</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog.py" line="62"/>
+        <source>REP/ETC</source>
+        <translation>トゥルーカラー再現画像</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog.py" line="62"/>
+        <source>SND/ETC</source>
+        <translation>雲頂強調画像</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog.py" line="139"/>
+        <source>Himawari (Full disk)</source>
+        <translation>ひまわり（フルディスク）</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog.py" line="115"/>
+        <source>(Forecast)</source>
+        <translation>（予報）</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog.py" line="155"/>
+        <source>Himawari (Japan)</source>
+        <translation>ひまわり（日本付近）</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog.py" line="166"/>
+        <source>Now Cast</source>
+        <translation>雨雲の動き</translation>
+    </message>
+</context>
+<context>
+    <name>JMATilesLoaderDialogBase</name>
+    <message>
+        <location filename="../jma_tiles_loader_dialog_base.ui" line="14"/>
+        <source>JMA Tiles Loader</source>
+        <translation>JMA Tiles Loader</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog_base.ui" line="34"/>
+        <source>Himawari (Full disk)</source>
+        <translation>ひまわり（フルディスク）</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog_base.ui" line="156"/>
+        <source>TargetTime:</source>
+        <translation>撮影時間：</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog_base.ui" line="168"/>
+        <source>🔄</source>
+        <translation>🔄</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog_base.ui" line="119"/>
+        <source>Band:</source>
+        <translation>種類：</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog_base.ui" line="92"/>
+        <source>Himawari (Japan)</source>
+        <translation>ひまわり（日本付近）</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog_base.ui" line="150"/>
+        <source>Now Cast</source>
+        <translation>雨雲の動き</translation>
+    </message>
+    <message>
+        <location filename="../jma_tiles_loader_dialog_base.ui" line="177"/>
+        <source>Opacity:</source>
+        <translation>不透明度：</translation>
+    </message>
+</context>
+</TS>
